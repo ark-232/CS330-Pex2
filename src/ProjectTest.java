25215d8c0f5ac5d4f576b78e10a550df1eb76060
ark-232 <arkady232@gmail.com>
2023-10-03 10:20:29 -0600

    edited actions

--- src/ProjectTest.java
+++ src/ProjectTest.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class ProjectTest {
+public class ProjectTest {
+
+    public static void main(String[] args) {
+        testClassesExist();
+        testDrawingPanelUsage();
+        // Add more tests as needed
+    }
+
+    static void testClassesExist() {
+        try {
+            Class.forName("Main");
+            Class.forName("Battle");
+            Class.forName("Army");
+            Class.forName("Warrior");
+            Class.forName("Vector330");
+            Class.forName("DrawingPanel");
+            // Add other class checks
+            System.out.println("All required classes exist.");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Some required classes are missing!");
+            System.exit(1);
+        }
+    }
+
+    static void testDrawingPanelUsage() {
+        // Here you can add some logic to check if DrawingPanel is used correctly
+        // This might include instantiating some of your classes and invoking methods
+        // For example:
+        // Battle battle = new Battle();
+        // battle.start(); // or any relevant method
+        // And then check some conditions to verify its behavior
+    }
+
+    // You can add more tests for other requirements
 }
